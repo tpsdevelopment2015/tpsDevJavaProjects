ba2ba8f08c31e5961203dff6f2e7b2b13a7504da
Tim <Tim@tpsDevelopment>
2017-09-27 20:19:59 -0500

    initial commit

---
+++ VocabularyFun/src/VocabularyFun.java
@@ -0,0 +1,12 @@
+
+public class VocabularyFun {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}//end mail 
+
+}//end class
---
+++ VocabularyFun/src/Word.java
@@ -0,0 +1,16 @@
+
+public class Word {
+	
+	String word, definition;
+	
+	private Word(String word, String definition)
+	{	
+		this.word = word;
+		this.definition = definition;	
+	}//end constructor
+
+	
+	
+	
+	
+}//end class
